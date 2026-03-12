0f5d576600d5a7c356b123cb40b4d2868f3cf8f0
UmidHajiyev <umid.haciyev.2001@gmail.com>
2026-03-12 17:34:11 +0400

    Added home endpoint

--- src/main/java/com/umid/instalike/controller/HelloController.java
+++ src/main/java/com/umid/instalike/controller/HelloController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String home()
+    {
+        return "InstaLike is running!";
+    }
+
     @GetMapping("/hello")
     public String sayHello()
     {
