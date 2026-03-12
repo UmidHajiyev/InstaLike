eaa5efd4fe33022475579e285597d7ecc757f801
UmidHajiyev <umid.haciyev.2001@gmail.com>
2026-03-12 15:28:09 +0400

    Initial Spring Boot Project Set Up

---
+++ src/main/java/com/umid/instalike/InstaLikeApplication.java
@@ -0,0 +1,13 @@
+package com.umid.instalike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaLikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umid/instalike/InstaLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umid.instalike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaLikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
